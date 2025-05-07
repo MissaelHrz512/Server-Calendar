dcce3b91e00003ac8d17dd919dc4ad8ff96f5a1d
MissaelHrz512 <ubunto67@gmail.com>
2025-05-07 12:23:31 -0600

    Create Event

--- controllers/events.js
+++ controllers/events.js
@@ -1,18 +1,30 @@
 //CRUD OPERATIONS
+const Event = require("../models/Event");
 
-const getEvents = (req, res) => {
+const getEvents = async(req, res) => {
+  //Retorna el id y el name del usuario
+  const events =await Event.find().populate('user', 'name');
   res.status(200).json({
     ok: true,
-    msg: "Events",
+    events: events,
   });
 };
 
-const createEvent = (req, res) => {
-  res.status(200).json({
-    ok: true,
-    msg: "Create",
-    event: req.body,
-  });
+const createEvent = async (req, res) => {
+  try {
+    const event = new Event({ ...req.body, user: req.user.uid });
+    const userEvent=await event.save();
+    return res.status(200).json({
+      ok: true,
+      msg: "Created",
+      event: userEvent,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Call Admin",
+    });
+  }
 };
 
 const updateEvent = (req, res) => {
